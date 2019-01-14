5f99ec43651c928b9242dd5bccbc88e2563aeec8
namviet2612 <34891575+namviet2612@users.noreply.github.com>
2019-01-14 23:58:43 +0700

    Add files via upload

---
+++ include/FontSetup.h
@@ -0,0 +1,23 @@
+/****************************************************************************
+Font created by the LCD Vision V1.05 font & image editor/converter
+(C) Copyright 2011-2013 Pavel Haiduc, HP InfoTech s.r.l.
+http://www.hpinfotech.com
+
+Font name: .VnArial Bold
+The font is proportional (has variable width).
+Font height: 16 pixels
+First character: 0x20
+Last character: 0x7F
+
+Exported font data size:
+1988 bytes for displays organized as horizontal rows of bytes
+1460 bytes for displays organized as rows of vertical bytes.
+****************************************************************************/
+
+#ifndef _FONT_SETUP_INCLUDED_
+#define _FONT_SETUP_INCLUDED_
+
+extern const unsigned char font_setup[];
+
+#endif
+
---
+++ include/lcd.h
@@ -0,0 +1,27 @@
+#ifndef LCD_H
+#define LCD_H
+
+#include <math.h>
+#include "font.h"
+#include "FontSetup.h"
+#include "FontTenCT.h"
+#include "LPC17xx.h"
+
+void GLCD_Delay(void);               //delay 16 chu ky may
+void GLCD_OUT_Set(void);                //Set huong Out cho cac chan
+void GLCD_IN_Set(void);               //Set huong In cho cac chan
+void GLCD_SetSide(unsigned char Side);        //Chon chip KS0108 trai hoac phai
+void wait_GLCD(void);                //cho GLCD ranh
+void GLCD_SetDISPLAY(uint8_t ON);    //Cho phep hien thu GLCD
+void GLCD_SetYADDRESS(uint8_t Col);    //Set dia chi cot Y
+void GLCD_SetXADDRESS(uint8_t Line);    //set dia chi page X
+void GLCD_StartLine(uint8_t Offset);    //set start line khi cuon GLCD
+void GLCD_WriteDATA(uint8_t DATA);    //ghi du lieu vao GLCD
+uint8_t GLCD_ReadDATA(void);            //doc du lieu tu GLCD
+void GLCD_Init(void);                //khoi dong GLCD
+void GLCD_GotoXY(uint8_t Line, uint8_t Col);    //Di chuyen den vi tri co to do page, cot tren man hinh
+void GLCD_Clr(void);                            //Xoa toan bo GLCD
+void GLCD_PutChar78(uint8_t Line, uint8_t Col, uint16_t chr);        //In ky tu kich thuoc 7x8
+void GLCD_Print78(uint8_t Line, uint8_t Col, char* str);            //In chuoi ky tu 7x8
+void GLCD_PutBMP(uint8_t * bmp, uint8_t x, uint8_t y);                                        //in hinh anh len GLCD
+#endif
